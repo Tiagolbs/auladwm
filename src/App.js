4f7a2e6bd3f492774b89af1d4d4c768019b4017a
Tiago Seben <tiagolbs31@gmail.com>
2020-09-17 21:40:49 -0300

    Att

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            lista: [
+
+            ]
+        }
+
+        this.onClickAtualizar = this.onClickAtualizar.bind(this);
+    }
+
+    onClickAtualizar(){
+        fetch("http://localhost:3001/users")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        lista: result
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <h4>Users</h4>
+
+                <button onClick={this.onClickAtualizar}
+                        type="button">
+                    Atualizar Lista
+                </button>
+
+
+                <table className="table">
+                    {
+                        this.state.lista.map(function (p) {
+                                return (
+                                    <tr>
+                                        <td> {p.id} </td>
+                                        <td> {p.nome} </td>
+                                        <td> {p.senha} </td>
+                                        <td> {p.email} </td>
+                                        <td> {p.celular} </td>
+                                    </tr>
+                                );
+
+                            }
+
+                        )
+
+                    }
+                </table>
+            </div>
+        );
+    }
 }
 
 export default App;
